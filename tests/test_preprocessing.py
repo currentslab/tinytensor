c939ca9d9dc78f630f9b8aec8a33f8ae81a13671
theblackcat102 <theblackcat102@github.com>
2022-10-07 21:59:47 +0800

    add support for huggingface tokenizer format

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,24 +1,35 @@
 import numpy as np
 from tinytensor.postprocessing.classification import HierarchicalMultiClassification
 from tinytensor.math_utils import sigmoid
-
+from tinytensor.preprocessing.tokenizer import TextProcessing
 
 
 def test_hierarchical():
     class_mapping = 'models/multi-transformers-epoch-09/class2idx.json'
     configuration = {
+        "mapping": "class2idx.json",
         'level_class': [27, 644],
+        '_dir': 'models/multi-transformers-90k',
         'threshold': 0.5
     }
-    postprocessing = HierarchicalMultiClassification(class_mapping, configuration)
+    postprocessing = HierarchicalMultiClassification(configuration)
 
 
     logits = -np.ones((32, 644))
     logits[:3, :100] =  np.random.random((3, 100))
     logits[:3, :10] = np.ones((3, 10))
-    outputs = {
-        'hierarchical': sigmoid(logits)
-    }
-    output = postprocessing.forward(outputs, {})
+    output = postprocessing(sigmoid(logits), {})
     for zero_output in output[3:]:
         assert len(zero_output) == 0
+
+
+def test_tokenizer():
+    preprocessing = TextProcessing({
+        '_dir': 'models/multi-transformers-90k',
+        'tokenizer_file': 'tokenizer-90k.json',
+        'name': 'tokens',
+        'pad_token': '[PAD]'
+    })
+    inputs = {"tokens": ["Apple 3rd quadter earnings had imrpive", "I have high probability to assume you had nothing to do"]}
+    output = preprocessing(inputs, {})
+    assert len(output['tokens'].shape) > 1
--- tinytensor/preprocessing/tokenizer.py
+++ tinytensor/preprocessing/tokenizer.py
@@ -15,15 +15,36 @@ class TextProcessing(Preprocessing):
             os.path.join(configuration['_dir'], configuration['tokenizer_file'])
         )
         self.name = configuration["name"]
+        self.return_attention = False
+        if 'return_attention' in configuration:
+            self.return_attention = configuration['return_attention']
+        self.pad_token = self.tokenizer.token_to_id(configuration["pad_token"])
 
     def __call__(self, inputs, model_inputs):
-        text = inputs[self.name]
-        if isinstance(text, str):
-            text = [text]
+        texts = inputs[self.name]
+        if isinstance(texts, str):
+            texts = [texts]
 
-        tokens = np.array([ encode.ids \
-                for encode in self.tokenizer.encode_batch(text) ])
-        model_inputs[self.name] = tokens
+        output = []
+        max_length = 0
+        for text in texts:
+            encode = self.tokenizer.encode(text)
+            output.append(encode.ids)
+            max_length = max(len(encode.ids), max_length)
+        np_output = []
+        attn_output = []
+        for token in output:
+            t_len = len(token)
+            pad_len = max(max_length-t_len, 0)
+            if pad_len > 0:
+                token += [self.pad_token]*pad_len
+            np_output.append(token)
+            if self.return_attention:
+                attn_output.append([1]*t_len + [0]*pad_len)
+
+
+        if self.return_attention:
+            model_inputs['attention_mask'] = np.array(attn_output)
+        model_inputs[self.name] = np.array(np_output)
         return model_inputs
-
 
